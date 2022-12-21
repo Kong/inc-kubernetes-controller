b2d57b5436713d9fd7ad7882e233cb2a287900b3
Travis Raines <571832+rainest@users.noreply.github.com>
2022-12-21 13:44:35 -0800

    chore: remove stub KongPlugin controller

--- main.go
+++ main.go
@@ -133,15 +133,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// TODO
-	//if err = (&configurationkonghqcomcontrollers.KongPluginReconciler{
-	//	Client: mgr.GetClient(),
-	//	Scheme: mgr.GetScheme(),
-	//	Store:  &services,
-	//}).SetupWithManager(mgr); err != nil {
-	//	setupLog.Error(err, "unable to create controller", "controller", "KongPlugin")
-	//	os.Exit(1)
-	//}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
